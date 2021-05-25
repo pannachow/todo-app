8f431b31187488891943f4a8992d1b1ab9393487
pannachow <chowpanna@gmail.com>
2021-05-25 11:37:49 +0200

    Updates

---
+++ precache-manifest.abe076b766e392acbb5f4aafda7b08d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5f83bdd2094e81b6ae9c65960575ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a5d696c8d9feee55a03",
+    "url": "./static/js/2.18340d98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.18340d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a32ca672ad6f1f91de2",
+    "url": "./static/js/main.45cb9266.chunk.js"
+  },
+  {
+    "revision": "3fe9786d0a7b3541a938",
+    "url": "./static/js/runtime-main.5b8fdfe4.js"
+  }
+]);
